374a62b5fffbf6f7f50850bc28d39b2c2df68e76
MikoFahreza <mikoyusrizal@gmail.com>
2023-03-25 20:48:10 +0700

    tabel transaksi

---
+++ database/migrations/2023_03_20_091837_create_transaksi_table.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+ /**
+ * Run the migrations.
+ *
+ */
+ public function up():void
+ {
+ Schema::create('transaksi', function (Blueprint $table) {
+    $table->increments('invoice');
+    $table->unsignedInteger('id_jadwal');
+    $table->foreign('id_jadwal')->references('id_jadwal')->on('jadwal')->onDelete('cascade');
+    $table->unsignedInteger('id_metode_pembayaran');
+    $table->foreign('id_metode_pembayaran')->references('id_metode_pembayaran')->on('metode_pembayaran')->onDelete('cascade');
+    $table->unsignedInteger('id_user');
+    $table->foreign('id_user')->references('id_user')->on('user')->onDelete('cascade');
+    $table->timestamps('waktu');
+ });
+ }
+ /**
+ * Reverse the migrations.
+ *
+ * @return void
+ */
+ public function down(): void
+ {
+ Schema::dropIfExists('kursi');
+ }
+};
